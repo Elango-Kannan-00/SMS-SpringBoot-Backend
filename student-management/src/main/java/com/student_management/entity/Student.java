74dfac3d6f14e58afce8b7a49379256f74780ed8
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-02 23:41:02 +0530

    Added entity file

--- student-management/src/main/java/com/student_management/entity/Student.java
+++ student-management/src/main/java/com/student_management/entity/Student.java
@@ -10,8 +10,8 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     // Student fields
-  
     private String name;
     private String email;
     private String department;
