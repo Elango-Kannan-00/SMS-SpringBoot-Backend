f64dfa5a20ca074f5bd141329dfafba9dc5b1175
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-02 23:41:48 +0530

    Added repository file

--- student-management/src/main/java/com/student_management/repository/StudentRepository.java
+++ student-management/src/main/java/com/student_management/repository/StudentRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 import com.student_management.entity.Student;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    
-}
+public interface StudentRepository extends JpaRepository<Student, Long> { }
