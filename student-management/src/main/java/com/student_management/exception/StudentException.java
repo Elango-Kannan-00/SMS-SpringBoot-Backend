80c17fe718100f7c71596ec56926e9af540233d3
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-05 20:16:42 +0530

    Added exception file

--- student-management/src/main/java/com/student_management/exception/StudentException.java
+++ student-management/src/main/java/com/student_management/exception/StudentException.java
@@ -1,5 +1,22 @@
 package com.student_management.exception;
 
-public class StudentException extends Exception {
+public class StudentException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
+    public StudentException() {
+        super();
+    }
+
+    public StudentException(String message) {
+        super(message);
+    }
+
+    public StudentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentException(Throwable cause) {
+        super(cause);
+    }
 }
